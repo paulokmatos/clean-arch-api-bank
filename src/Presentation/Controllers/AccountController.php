88f1b4c8dd041ed508271d2114f7a0eae1750e98
paulokmatos <kendako361@gmail.com>
2024-06-30 23:27:46 -0300

    feat: implementing create account route with persistence in mysql db.

---
+++ src/Presentation/Controllers/AccountController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Presentation\Controllers;
+
+use App\Application\UseCases\CreateAccountUseCase;
+use App\Presentation\Routers\RouterDispatcher\JsonResponse;
+use App\Presentation\Routers\RouterDispatcher\Request;
+
+readonly class AccountController
+{
+    public function __construct(private CreateAccountUseCase $createAccountUseCase)
+    {
+        //
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function create(Request $request): JsonResponse
+    {
+        $accountNumber = $request->get('numero_conta');
+        $amount = $request->get('saldo');
+
+        if (!$accountNumber || !$amount) {
+            throw new \Exception("required parameter 'numero_conta' and 'saldo'");
+        }
+
+        if (!is_numeric($amount)) {
+            throw new \Exception("'saldo' must be numeric of type float");
+        }
+
+        if (!is_numeric($accountNumber) || is_float($accountNumber)) {
+            throw new \Exception("'numero_conta' must be numeric of type int");
+        }
+
+        $account = $this->createAccountUseCase->execute($accountNumber, $amount);
+
+        return new JsonResponse(['numero_conta' => $account->accountNumber], 201);
+    }
+}
